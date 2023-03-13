21ab6da8ec22f8d8f109253eff69cca0efeedc52
Sergiom8m <81020872+Sergiom8m@users.noreply.github.com>
2023-03-13 11:13:46 +0100

    First Request

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+import getpass
+import signal
+import sys
+import urllib
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from bs4 import BeautifulSoup
+from pip import main
+
+user = ''
+nameSurname = ''
+password = ''
+cookie = ''
+loginToken = ''
+uriRequest = ''
+pdfcount = 0
+
+def firstRequest():
+
+    ## ASK FOR INDEX.PHP PAGE
+    method = "GET"
+    uri = "https://egela.ehu.eus/login/index.php"
+    headers = {'Host': 'egela.ehu.eus'}
+
+    print("---------------LEHEN ESKAERA--------------\n")
+
+    print(method + "\n")
+    print(uri + "\n")
+
+    print("---------------LEHEN ESKAERAREN ERANTZUNA--------------\n")
+
+    response = requests.request(method, uri, headers=headers, allow_redirects=False)
+
+    code = response.status_code
+    description = response.reason
+    print("STATUS: " + str(code) + " DESKRIBAPENA: " + description)
+    content = response.content
+
+    ## GET THE COOKIE VALUE FROM INDEX.PHP PAGE
+    Cookie = response.headers['Set-Cookie'].split(";")[0]
+    print("Set-Cookie: " + Cookie + "\n")
+
+    ##
+    document = BeautifulSoup(content, 'html.parser')
+    token = document.find('input', {'name': 'logintoken'})
+    if (token.has_attr('value')):
+        logintoken = token["value"]
+        print("Logintoken: " + logintoken + "\n")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    firstRequest();
