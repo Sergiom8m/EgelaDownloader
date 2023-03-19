1393609f1ffb48db536db0e7cf2731596953dac9
Sergiom8m <81020872+Sergiom8m@users.noreply.github.com>
2023-03-19 12:08:46 +0100

    First Request

--- main.py
+++ main.py
@@ -16,68 +16,77 @@ uriRequest = ''
 pdfcount = 0
 
 
-def firstRequest():
-    # ASK FOR INDEX.PHP PAGE
-    method = "GET"
-    uri = "https://egela.ehu.eus/login/index.php"
-    headers = {'Host': 'egela.ehu.eus'}
-
-    print("---------------LEHEN ESKAERA--------------\n")
-
-    print(method + "\n")
-    print(uri + "\n")
-
-    print("---------------LEHEN ESKAERAREN ERANTZUNA--------------\n")
-
-    response = requests.request(method, uri, headers=headers, allow_redirects=False)
-
-    code = response.status_code
-    description = response.reason
-    print("STATUS: " + str(code) + " DESKRIBAPENA: " + description)
-    content = response.content
-
-    # GET LOGIN TOKEN VALUE
-    document = BeautifulSoup(content, 'html.parser')
-    token = document.find('input', {'name': 'logintoken'})
-    if token.has_attr('value'):
-        logintoken = token["value"]
-        print("Logintoken: " + logintoken + "\n")
-
-    # GET THE COOKIE VALUE FROM INDEX.PHP PAGE
-    Cookie = response.headers['Set-Cookie'].split(";")[0]
-    print("Set-Cookie: " + Cookie + "\n")
-
-    # GET THE URI FOR THE NEXT REQUEST
-    if ('Location' in response.headers) is False:
-        uriRequest = uri
-
-    print("URI REQUEST ", uriRequest)
-
 def data_request():
     # MAKE THE VARIABLES GLOBAL TO BE ACCESSIBLE FROM EVERYWHERE
     global user
     global password
     global nameSurname
 
-    if len(sys.argv) == 3:
-        user = sys.argv[1]
-        nameSurname = sys.argv[2].upper()  # CAPITALIZE THE STRING
-
-    else:
+    if len(sys.argv) != 3:
         print("ERROR! YOU HAVE INTRODUCED A SINGLE ARGUMENT OR MORE THAN 2 ARGUMENTS")
         print("INTRODUCE 2 ARGUMENTS. FOR EXAMPLE: python.exe nameProgram.py user \"name surname\"")
         sys.exit(0)
 
-    print(user, nameSurname)
+    else:
+        user = sys.argv[1]
+        nameSurname = sys.argv[2].upper()  # CAPITALIZE THE STRING
+
+    print("ENTERED USER AND NAME --> " + user + ", " + nameSurname);
 
     try:
-        password = getpass.getpass(prompt='Password: ', stream=None)
+        password = getpass.getpass(prompt='PASSWORD: ', stream=None)
     except Exception as error:
         print('ERROR', error)
     else:
-        print('Password entered: ', password)
+        print('PASSWORD ENTERED --> ', password)
 
+
+def firstRequest():
+
+    #########################################################################
+    # GET /login/index.php HTTP / 1.1
+    # Host: egela.ehu.eus
+    #########################################################################
+
+    # MAKE THE VARIABLES GLOBAL TO BE ACCESSIBLE FROM EVERYWHERE
+    global cookie
+    global loginToken
+    global uriRequest
+
+    # SET THE REQUEST
+    method = 'GET'
+    uri = "https://egela.ehu.eus/login/index.php"
+    headers = {'Host': 'egela.ehu.eus'}
+
+    # GET REQUEST'S RESPONSE
+    response = requests.get(uri, headers=headers, allow_redirects=False)
+    code = response.status_code
+    description = response.reason
+    print("1º REQUEST'S METHOD AND URI --> " + method + " " + uri)
+    print("1º REQUEST --> " + str(code) + " " + description)
+
+    # GET THE COOKIE VALUE
+    cookie = response.headers['Set-Cookie'].split(';')[0]
+    print("1º REQUEST'S COOKIE --> " + cookie)
+
+    # CHECK IF LOCATION URI EXISTS OR NOT
+    if ('Location' in response.headers) is False:
+        uriRequest = uri
+
+    # PRINT THE URI FOR THE NEXT REQUEST
+    print("URI REQUEST --> " + uriRequest)
+
+    # GET THE REQUEST CONTENT (HTML)
+    html = response.content
+
+    # PARSE THE HTML CODE TO GET LOGINTOKEN VALUE
+    soup = BeautifulSoup(html, 'html.parser')
+    token = soup.find('input', {'name': 'logintoken'})
+
+    if token.has_attr('value'):
+        loginToken = token['value']
+        print("LOGIN TOKEN --> ", loginToken)
 
 if __name__ == '__main__':
-    firstRequest();
     data_request();
+    firstRequest();
